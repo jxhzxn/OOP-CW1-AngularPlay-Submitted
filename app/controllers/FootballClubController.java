0b724f01056abbe9c1dfb371e49bf3fc37b6d6fe
jxhzxn <jahzanahamed99@gmail.com>
2020-12-24 23:08:55 +0530

    ADDED: added club retrieving

--- app/controllers/FootballClubController.java
+++ app/controllers/FootballClubController.java
@@ -2,16 +2,12 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import entities.FootballClub;
-import entities.Person;
 import entities.PremierLeagueManager;
 import play.libs.Json;
 import play.mvc.Result;
 import service.FootballClubService;
 import utils.ApplicationUtil;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Set;
 
 import static play.mvc.Results.created;
@@ -33,7 +29,7 @@ public class FootballClubController{
         return ok(jsonNode).as("application/json");
     }
 
-    public Result getClub(){
+    public Result getAllClubs(){
 //        PremierLeagueManager premierLeagueManager = new PremierLeagueManager();
 //        PremierLeagueManager plm = premierLeagueManager.plmCheck();
 //        JsonNode clubJson = Json.toJson(plm.getClubsArray().get(1));
@@ -41,7 +37,16 @@ public class FootballClubController{
 
         FootballClubService fcs = new FootballClubService();
 
-        Set<FootballClub> result = fcs.getAllEmployees();
+        Set<FootballClub> result = fcs.getAllClubs();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonData = mapper.convertValue(result, JsonNode.class);
+        return ok(ApplicationUtil.createResponse(jsonData, true));
+    }
+
+    public Result getClub(String name){
+        FootballClubService fcs = new FootballClubService();
+
+        Set<FootballClub> result = fcs.getClub(name);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonData = mapper.convertValue(result, JsonNode.class);
         return ok(ApplicationUtil.createResponse(jsonData, true));
--- app/service/FootballClubService.java
+++ app/service/FootballClubService.java
@@ -3,15 +3,30 @@ package service;
 import entities.FootballClub;
 import entities.PremierLeagueManager;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FootballClubService {
 
-    public Set<FootballClub> getAllEmployees() {
+    public Set<FootballClub> getAllClubs() {
         PremierLeagueManager premierLeagueManager = new PremierLeagueManager();
         PremierLeagueManager plm = premierLeagueManager.plmCheck();
         return new HashSet<>(plm.getClubsArray());
+    }
+
+    public HashSet getClub(String name) {
+        PremierLeagueManager premierLeagueManager = new PremierLeagueManager();
+        PremierLeagueManager plm = premierLeagueManager.plmCheck();
+        ArrayList<HashSet> array = new ArrayList<>();
+        for(FootballClub club: plm.getClubsArray()){
+            if(club.getClubName().equals(name)){
+                HashSet<FootballClub> hashSet = new HashSet<>();
+                hashSet.add(club);
+                array.add(hashSet);
+            }
+        }
+        return array.get(0);
     }
 
 }
