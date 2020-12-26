bbbf13f34c80f527bf8f800eafddfe08216ed5b5
jxhzxn <jahzanahamed99@gmail.com>
2020-12-26 23:46:40 +0530

    last commit on 26DEC2020

--- ui/src/app/add-match/add-match.component.ts
+++ ui/src/app/add-match/add-match.component.ts
@@ -19,16 +19,22 @@ export class AddMatchComponent implements OnInit {
   }
 
   public addMatch(): void{
+    
     if(this.goal1==null){
       this.goal1=0;
     }
     if(this.goal2==null){
       this.goal2=0;
     }
+
+    if(this.clubName1 == null || this.clubName2 == null){
+      alert("Fill Everything");
+    }else{
       this.addMatchService.addMatch(this.clubName1,this.clubName2,this.goal1,this.goal2).subscribe((data: any) => {
         alert("success");
-        console.log(data.content);
       })
+    }
+
   }
 
 }
